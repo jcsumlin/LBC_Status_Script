62d1b943373db212119034829f6a783f20728e39
J_C___ <jcsumlin@gmail.com>
2018-09-13 20:47:56 -0500

    updated myself

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import LocalBitcoin
 import os
+# import configparser
 import time
 
+# config = configparser.ConfigParser()
+# config.read('auth.ini')
 hmac_auth_key = os.environ['hmac_auth_key']
 # hmac_auth_key = str(config.get('auth', 'hmac_auth_key'))  # REQUIRED
 hmac_auth_secret = os.environ['hmac_auth_secret']
@@ -12,16 +15,17 @@ min_counter = 13
 
 try:
     myself = lc.getMyself()
-    print("\r\nHello, " + myself['data']['username'] + ".\r\n")
+    print("\r\nHello, " + myself['username'] + ".\r\n")
 except Exception as e:
     print(
-        "Error authenticating API KEY + API SECRET, or error parsing data. Please try again.\n\n {e}".format(e=e))
+        "Error authenticating API KEY + API SECRET, or error parsing data. Please try again.\n\n")
+    print(e)
     exit()
 
 while True:
     min_counter -= 1
     if (min_counter == 0):
-        print('Refreshing online status for ' + myself['data']['username'] + '.')
+        print('Refreshing online status for ' + myself['username'] + '.')
         lc.getMyself()
         min_counter = 13
     else:
